ceb8d25db3bf38b4bcb9e6ab94eba1f62416f890
“Marcela <mpmontiela@gmail.com>
2022-10-24 17:06:55 +0200

    done

--- javascript/index.js
+++ javascript/index.js
@@ -39,10 +39,8 @@ getInstruction("mashedPotatoes", 0, (step1) => {
           document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`,
           getInstruction("mashedPotatoes", 4, (step5) => {
             document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`,
-            getInstruction("mashedPotatoes", 5, (step6) => {
-              document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
               document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-            }, (error) => console.log(error));
           }, (error) => console.log(error));
         }, (error) => console.log(error));
       },
